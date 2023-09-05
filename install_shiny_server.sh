7dffc1c7bfc06f9f58a8d6d9fedd1b249ac48b99
jlstevens <jlrstevens@gmail.com>
2023-09-05 14:17:24 +0200

    Completed first full cut of install_shiny_server.sh

--- install_shiny_server.sh
+++ install_shiny_server.sh
@@ -46,3 +46,50 @@ elif [ ! -w $SHINY_SERVER_PARENT ]; then
     echo "in README.md to set it to read-write, and stop and restart this session."
     exit -1
 fi
+
+
+if [[ ! -f ss-centos7.rpm && ! -f data/ss-centos7.rpm ]]; then
+    echo 'ERROR: the Shiny Server binaries are not present. Please follow the'
+    echo 'directions in README.md to bring these binaries into the project.'
+    exit -1
+fi
+
+
+fname=ss-centos7.rpm
+[ -f $fname ] || fname=data/$fname
+echo "- Verifying $fname"
+if ! rpm2cpio $fname >/dev/null; then
+    echo "- ERROR: $fname is not a valid RPM package. Please remove this file and re-download it."
+    exit -1
+fi
+
+echo "- Install prefix: $SHINY_SERVER_PREFIX"
+
+
+echo "- Staging RHEL7/CentOS7 binary"
+mkdir -p $SHINY_SERVER_PREFIX/staging7
+fname=ss-centos7.rpm
+[ -f $fname ] || fname=data/$fname
+rpm2cpio $fname > $SHINY_SERVER_PREFIX/staging7/ss-centos7.cpio
+cd $SHINY_SERVER_PREFIX/staging7 && cpio -id < $SHINY_SERVER_PREFIX/staging7/ss-centos7.cpio
+
+echo "- Moving files into final position"
+mv $SHINY_SERVER_PREFIX/staging7/opt/shiny-server/* $SHINY_SERVER_PREFIX
+rm -rf $RSTUDIO_PREFIX/staging7
+
+
+echo "+-----------------------+"
+echo "Shiny Server installation is complete."
+echo "Once you have verified the installation, feel free to"
+echo "shut down this session and delete the project."
+echo "+-----------------------+"
+[ -z "$CONDA_PREFIX" ] || source deactivate
+java_loc=$(which java 2>/dev/null)
+if [ -z "$java_loc" ]; then
+    echo "WARNING: Many R packages make use of Java, and it seems"
+    echo "not to be present on this installation of AE5. To make"
+    echo "Java available to all AE5 users, run install_java.sh, or"
+    echo "manually download a JDK Linux x64 archive and unpack its"
+    echo "contents into the directory /tools/java."
+echo "+-----------------------+"
+fi
